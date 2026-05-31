dec1f6fabb61da151bb096f01f575271a129e453
Tehgan Porter <tehgan@proton.me>
2026-05-31 16:01:48 -0400

    Final pre-Bootstrap version

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // TODO: Profile performance versus 'old-fashioned' way (scroll events)
 const HEADER = document.querySelector("header");
 const OBSERVER = new IntersectionObserver(((entries) => {
-    $(".navbar-container").toggleClass("sticky-navbar", !entries[0].isIntersecting);
+    $(".navbar-container").toggleClass("navbar-stickifier", !entries[0].isIntersecting);
 }));
 
 OBSERVER.observe(HEADER);
