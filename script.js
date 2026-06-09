b514460ac88f8c05af27dca2255a8f4f6c20bc32
Tehgan Porter <tehgan@proton.me>
2026-06-09 18:33:57 -0400

    Fix font loading in live environment (hopefully)

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // Sticky navbar
 // https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API#overview
-// TODO: Profile performance versus 'old-fashioned' way (scroll events)
 const HEADER = document.querySelector("header");
 const OBSERVER = new IntersectionObserver(((entries) => {
     $(".navbar-container").toggleClass("navbar-stickifier", !entries[0].isIntersecting);
